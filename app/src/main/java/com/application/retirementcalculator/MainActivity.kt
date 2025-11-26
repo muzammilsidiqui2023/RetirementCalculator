ccfa4c3806c0de53aa8f44dd025d61b0803e3909
muzammil <muzammil.siddiqui@branex.com>
2025-11-26 11:47:33 +0500

    changes

--- app/src/main/java/com/application/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/application/retirementcalculator/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RetirementCalculatorTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
